ef3991dc3bfa2e8e50ade872e3d2f103524e5922
JoeBiggia <jbaki91@yahoo.com>
2020-01-02 15:54:25 -0500

    Added favicons

--- components/sitehead/SiteHead.jsx
+++ components/sitehead/SiteHead.jsx
@@ -34,25 +34,23 @@ class SiteHead extends PureComponent {
 				<meta name='description' content='The Joe Biggica portfolio site' />
 				<meta name='keywords' content='kung fu florida,kung fu fl,kung fu south fl,kung fu south florida,sifu fong,kwong wai lum,bill fong,william fong,sifu morrison,maurice morrison,hung gar,hung ga,hong jia,hong,chia,hung gar kung fu,hung ga kung fu,hung fist,frank yee,yee chee wai,kung fu,gung fu,gong fu,chi gong,tai chi,bagua,pa kua,baguazhang,ba gua zhang,chen tai chi,yang tai chi,taiji,shaolin,south shaolin,southern,lion dance,horse stance,wong fei hung,luk ah choi,wong kei ying,gee shim,abbot gee shim,hung hei gune,tang fong,tang fung,yuen ling,lam sai wing,tit kiu sam,wire,iron wire,dat mo,boddiharma,buddhist,karate,tiger,dragon,leopard,snake,crane,water,gold,metal,wood,fire,earth,five animals,5 animals,five elements,5 elements,self defence,iron palm,broadsword,pole,staff,butterfly swords,general kwan,guan gong,kwan ti,kwan gung,kwan yiu,liu bei,health,fitness,club,exercise,mind,body,spirit,chinese bodywork,qi gong,chi gung,meditation,inside kung fu,strecthing,chinese new year,alter,respect,discipline,mental concentration,throws,locking,techniques,eighteen lohan,new martial hero,self esteem,confidence,weapons,fighting,sparring,disabling,dim mak,mandarin,cantonese,china,chinese,ground fighting,tiger claw,hsing yi,shanghai,crane beak,fist,claw,paw,flying,ng,long,ba gua,gwun,gi mo cern do,hang yuet dan dao,cheunrg,tiger crane form,shaolin temple,monk,ba shing ba zu bagua zhang,free trial,private,complementary,group classes,forms,strikes,tiger fork,kwan dao,spear,kids,children,focus,school,zen,chan,black belt,long fist,yue jia siba fist,drunken fist,cha fist,luohan,long xing,lung,double broadswords,spring fall kwan dao,double end staff,huang long,meteor hammer,flying weight,fourth generation,tai shan,disciple,sensei,sifu,si gung,dai si gung,si jo,cum na,cum la sao,bong sao,tan sao,chee ji,sup ji kuen,ji jing san,dong hai chuan,wrestling,master,ngau gwat sin,successor,kung pao,real kung fu,dit da,drum,symbols,yees hung ga kung fu association,praying mantis,wing chun,united states traditional kung fu wushu federation,lessons,strength,yang style,push hands,ancestors,chi sao,ging,tae kwon do,fa jing,ki,lee,mok,choy,fut,hung,kwa,lau,sensitivity drills,sweat,pleasure,gung ji fuk fu,fu hok serng ying kuen,fu hok,double fist form,ng ying kuen,tiet sin kuen,fuk fu kuen,dat mo yit gan ging,fu jow gung,heavy bag,bag work,circle kick,kick,side kick,tiger tail kick,fu mei gerk,mo ying gerk,shadowless kick,ten killing hands,sup juet sao,eight strong techniques,ten tigers,eight cranes,eight stances,tiger and crane,champions,tournament wong fei hung,all kung fu,wong fei hung north eastern all kung fu championships,five formed fist,five animal fist,martial arts,chinese martial arts,chinese kung fu,ng ying,northern shaolin,chinatown new york,China,Hong Kong,Kuen,style,Martial Art,Sil Lum,Gung Fu,Wong Fei,Qi Gong,instruction,kwong,dojo,sihing,wing,simu,kwoon,movements,moves,palm,tradition,traditional,chi,fight,san shou,kickboxing,lohan,louhan,arhat,qigong,athletics,seminars,grappling,iron,hsing i,philosophy,taoism,wushu,buddhism,wah lum,tournament,competition' />
 
-				<meta name='twitter:title' content='Biggica.com' />
-				<meta name='twitter:site' content='@JoeBiggica' />
-				<meta name='twitter:creator' content='@JoeBiggica' />
 				<meta name='twitter:card' content='summary_large_image' />
-				<meta name='twitter:image' content='/static/biggica_logo.png' />
+				<meta name='twitter:image' content='/static/fongs-social-logo.png' />
 
-				<meta name='og:title' content='Biggica.com' />
-				<meta property='og:description' content='The Joe Biggica portfolio site' />
-				<meta property='og:image' content='/static/biggica_logo.png' />
-				<meta property='og:url' content='https://www.biggica.com/' />
+				<meta name='og:title' content='Fong\'s Hung Ga' />
+				<meta property='og:description' content='The official Fong\'s Hung Ga website' />
+				<meta property='og:image' content='/static/fongs-social-logo.png' />
+				<meta property='og:url' content='https://www.fongshungga.com/' />
 
-				<meta name='msapplication-TileColor' content='#ffffff' />
+				<meta name='msapplication-TileColor' content='#000000' />
 				<meta name='msapplication-config' content='none' />
-				<meta name='theme-color' content='#333333' />
+				<meta name='theme-color' content='#FF1815' />
 
 				<link rel='shortcut icon' type='image/x-icon' href='/static/favicon/favicon.png' />
 				<link rel='icon' type='image/png' href='/static/favicon/favicon.png' sizes='16x16' />
 				<link rel='icon' type='image/png' href='/static/favicon/favicon-32x32.png' sizes='32x32' />
 				<link rel='icon' type='image/png' href='/static/favicon/favicon-48x48.png' sizes='48x48' />
+				<link rel='icon' type='image/png' href='/static/favicon/favicon-96x96.png' sizes='96x96' />
 			
 				{this.props.meta.map(this.renderTag)}
 			</Head>
