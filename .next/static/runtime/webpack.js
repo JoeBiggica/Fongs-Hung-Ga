ec42032cec07349f42dfb210506e3ecc7cb42f18
JoeBiggia <jbaki91@yahoo.com>
2020-02-12 23:19:01 -0500

    Added fixed background

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e4fada2a798f0db01ed4";
+/******/ 	var hotCurrentHash = "877b72974d721b612cea";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
