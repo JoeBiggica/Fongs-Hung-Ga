2e58d99ce6bea660fb94213a29fbec48a3b323ac
JoeBiggia <jbaki91@yahoo.com>
2020-03-12 11:53:34 -0400

    Removed git caches

--- redux/makeStore.js
+++ redux/makeStore.js
@@ -1,13 +1,14 @@
 import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { apiMiddleware } from 'redux-api-middleware';
 
 import { reducer } from './reducers';
 
 export function initializeStore (initial_state = {}) {
 
-	return createStore(
-		reducer,
-		initial_state,
-		composeWithDevTools(applyMiddleware())
-	)
+	const createStoreWithMiddleware = applyMiddleware(apiMiddleware)(createStore);
+
+	const store = createStoreWithMiddleware(reducer, initial_state, composeWithDevTools());
+	
+	return store;
 }
