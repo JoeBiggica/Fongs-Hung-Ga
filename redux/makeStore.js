c72a4c7ccf3c34a924793c7fc0997a3fc527b06e
JoeBiggia <jbaki91@yahoo.com>
2020-03-12 14:58:40 -0400

    Removed api middleware

--- redux/makeStore.js
+++ redux/makeStore.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { apiMiddleware } from 'redux-api-middleware';
+// import { apiMiddleware } from 'redux-api-middleware';
 
 import { reducer } from './reducers';
 
 export function initializeStore (initial_state = {}) {
 
-	const createStoreWithMiddleware = applyMiddleware(apiMiddleware)(createStore);
+	const createStoreWithMiddleware = (createStore);
 
 	const store = createStoreWithMiddleware(reducer, initial_state, composeWithDevTools());
 	
